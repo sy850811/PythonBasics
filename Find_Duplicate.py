649ccbb9e4a0e7f1693df687b08e0b060bfc2b25
sy850811 <89657362+sy850811@users.noreply.github.com>
2021-08-28 00:30:49 +0530

    Create Find_Duplicate.py
    You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging from 0 to 3 and among these, there is a single integer value that is present twice. You need to find and return that duplicate number present in the array.
    Note :
    Duplicate number is always present in the given array/list.
    Input format :
    The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
    
    First line of each test case or query contains an integer 'N' representing the size of the array/list.
    
    Second line contains 'N' single space separated integers representing the elements in the array/list.
    Output Format :
    For each test case, print the duplicate element in the array/list.
    
    Output for every test case will be printed in a separate line.
    Constraints :
    1 <= t <= 10^2
    0 <= N <= 10^3
    Time Limit: 1 sec
    Sample Input 1:
    1
    9
    0 7 2 5 4 7 1 3 6
    Sample Output 1:
    7

---
+++ Find_Duplicate.py
@@ -0,0 +1,14 @@
+def find(li,i):
+    count=0
+    for e in li:
+        if e == i:
+            count+=1
+    return count == 2
+
+for i in range(int(input())):
+    n = int(input())
+    li = [int(x) for x in input().split()]
+    for i in range(n):
+        if((find(li,li[i]))):
+            print(li[i])
+            break
