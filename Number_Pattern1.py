f0303622f4ca93aa9dae117676bfd9b2db5f4018
sy850811 <89657362+sy850811@users.noreply.github.com>
2021-08-28 00:15:33 +0530

    Create Number_Pattern1.py
    Print the following pattern for n number of rows.
    For eg. N = 5
    
    1                1
    12            21
    123        321
    1234    4321
    1234554321

---
+++ Number_Pattern1.py
@@ -0,0 +1,19 @@
+n = int(input())
+i = 1
+while i<=n:
+    j=1
+    while j <= i:
+        print(j,end="")
+        j+=1
+    j=1
+    while j <= (n-i)*2:
+        print(" ",end="")
+        j+=1
+    j=1
+    p=i
+    while j <= i:
+        print(p,end="")
+        p-=1
+        j+=1
+    print()
+    i+=1
